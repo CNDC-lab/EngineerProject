f4f0fb7a4f29aa85d41c71c1a7f61d6f54d36baa
Mikolaj <m_zasada@me.com>
2022-04-25 19:56:53 +0200

    Initialization

---
+++ encryptor-api/src/main/java/pl/agh/edu/encryptorapi/EncryptorApiApplication.java
@@ -0,0 +1,12 @@
+package pl.agh.edu.encryptorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptorApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptorApiApplication.class, args);
+    }
+
+}
---
+++ encryptor-api/src/test/java/pl/agh/edu/encryptorapi/EncryptorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.encryptorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
